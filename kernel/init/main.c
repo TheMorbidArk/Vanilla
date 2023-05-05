d3d15a4f1594c9e2285b9670e5c43dc7b8bbe26a
TheMorbidArk <themorbidark@gmail.com>
2023-05-05 21:58:52 +0800

    Feat: 初步封装openSBI接口，SBI API - sbi_console_putchar

---
+++ kernel/init/main.c
@@ -0,0 +1,24 @@
+#include "sbi.h"
+void os_main()
+{
+    
+    sbi_console_putchar('H');
+    sbi_console_putchar('e');
+    sbi_console_putchar('l');
+    sbi_console_putchar('l');
+    sbi_console_putchar('o');
+    sbi_console_putchar(',');
+    sbi_console_putchar('R');
+    sbi_console_putchar('I');
+    sbi_console_putchar('S');
+    sbi_console_putchar('C');
+    sbi_console_putchar('-');
+    sbi_console_putchar('V');
+    sbi_console_putchar('\n');
+    
+    while (1) {
+
+    }
+    
+    return ;
+}
