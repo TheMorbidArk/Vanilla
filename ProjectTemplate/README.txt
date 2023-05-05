fd2d872cabfd931337c3f1b91e9414caec967f77
TheMorbidArk <themorbidark@gmail.com>
2023-05-05 15:31:52 +0800

    Feat: 项目架构初始化，Visual Studio 2022 窗口布局设定，Visual GDB项目模版建立

---
+++ ProjectTemplate/README.txt
@@ -0,0 +1,4 @@
+- Visual Studio 窗口布局
+QEMU for VisualGDB.vssettings
+- Visual GDB for K210 RISC-V in QEMU项目开发模版
+uCore Template to RISC-V.vgdbxpt
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "sbi.h"
 void os_main()
 {
-    SBI_PUTCHAR('H');
-    SBI_PUTCHAR('e');
-    SBI_PUTCHAR('l');
-    SBI_PUTCHAR('l');
-    SBI_PUTCHAR('o');
-    SBI_PUTCHAR('\n');
-    while(1) {}
-	return ;
+    while (1) {
+        SBI_PUTCHAR('H');
+        SBI_PUTCHAR('e');
+        SBI_PUTCHAR('l');
+        SBI_PUTCHAR('l');
+        SBI_PUTCHAR('o');
+        SBI_PUTCHAR('\n');
+    }
+    return ;
 }
